bafdb68a7f86febe7ba674505f0b0635a6102d08
Giulia Maccarone <giulia.maccarone02@gmail.com>
2022-10-13 14:23:16 +0200

    commit

---
+++ collections/counter.py
@@ -0,0 +1,14 @@
+from collections import Counter
+x = input()
+sz = input().split()
+N = int(input())
+buyer = []
+for _ in range(N):
+    buyer.append(input().split())
+pair = Counter(sz)
+money = 0
+for i in buyer:
+    if i[0] in pair.keys() and pair[i[0]] != 0:
+        money += int(i[1])
+        pair[i[0]] -= 1
+print(money)         
